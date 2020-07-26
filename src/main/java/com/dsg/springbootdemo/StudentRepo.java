6a552b51f08d04b613efcc247034b2b15ab782b0
Gaur <dg185245@ncr.com>
2020-07-26 20:05:38 +0530

    Return standard inline with student list.

--- src/main/java/com/dsg/springbootdemo/StudentRepo.java
+++ src/main/java/com/dsg/springbootdemo/StudentRepo.java
@@ -1,8 +1,10 @@
 package com.dsg.springbootdemo;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Component;
 
+@RepositoryRestResource(excerptProjection = InlineStandardProjection.class)
 public interface StudentRepo extends PagingAndSortingRepository<Student, Integer> {
 	
 }
