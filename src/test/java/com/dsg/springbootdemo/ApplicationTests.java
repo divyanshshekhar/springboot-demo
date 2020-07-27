9fea078eb9ef17d147fc3bb3649adbfe27cb23c3
Gaur <dg185245@ncr.com>
2020-07-27 12:38:42 +0530

    Optimized imports in ApplicationTests.java

--- src/test/java/com/dsg/springbootdemo/ApplicationTests.java
+++ src/test/java/com/dsg/springbootdemo/ApplicationTests.java
@@ -1,12 +1,7 @@
 package com.dsg.springbootdemo;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import com.dsg.springbootdemo.Application;
 
 @SpringBootTest
 class ApplicationTests {
